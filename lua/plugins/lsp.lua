0e754fdb16a56a6c0e6be6d841ab443284501d79
Getter Sethya <gettersethya@gmail.com>
2024-08-13 14:43:39 +0700

    fix: #2 remove K lsp keymap conflicted with default setting (maybe idk whyyy??)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -88,7 +88,6 @@ return {
 
 			local opts = { noremap = true, silent = true }
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
